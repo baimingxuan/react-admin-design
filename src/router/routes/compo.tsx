aeb2ecdb57fd883146efff7af15a0df6d7d48b37
baimingxuan <354372738@qq.com>
2023-04-11 18:10:20 +0800

    feat(router): Add compo route

---
+++ src/router/routes/compo.tsx
@@ -0,0 +1,60 @@
+import { AppRoute } from '../types'
+import { BasicLayout } from '../../layout'
+import Home from '../../views/home'
+
+// component module page
+const CompoRoute: AppRoute = {
+  path: '/compo',
+  element: <BasicLayout />,
+  meta: {
+      title: '组件',
+      icon: 'compo'
+  },
+  children: [
+      {
+          path: 'image-upload',
+          element: <Home />,
+          meta: {
+              title: '图片上传'
+          }
+      },
+      {
+          path: 'drag',
+          meta: {
+              title: '拖拽'
+          },
+          children: [
+              {
+                  path: 'drag-list',
+                  element: <Home />,
+                  meta: {
+                      title: '列表拖拽'
+                  }
+              },
+              {
+                  path: 'drag-resize',
+                  element: <Home />,
+                  meta: {
+                      title: '组件拖拽'
+                  }
+              }
+          ]
+      },
+      {
+          path: 'transfer',
+          element: <Home />,
+          meta: {
+              title: '穿梭框'
+          }
+      },
+      {
+          path: 'count-to',
+          element: <Home />,
+          meta: {
+              title: '数字滚动'
+          }
+      }
+  ]
+}
+
+export default CompoRoute
