43a3e09274ed06d3f05cab34cfae0b57c9d21c75
baimingxuan <354372738@qq.com>
2023-04-13 16:50:33 +0800

    feat(router): Create exception route

---
+++ src/router/routes/exception.tsx
@@ -0,0 +1,38 @@
+import { AppRoute } from '../types'
+import { BasicLayout } from '../../layout'
+import Home from '../../views/home'
+
+// exception module page
+const ExceptionRoute: AppRoute = {
+  path: '/exception',
+  element: <BasicLayout />,
+  meta: {
+    title: '异常页面',
+    icon: 'bug'
+  },
+  children: [
+    {
+      path: 'page-403',
+      element: <Home />,
+      meta: {
+        title: '403页面'
+      }
+    },
+    {
+      path: 'page-404',
+      element: <Home />,
+      meta: {
+        title: '404页面'
+      }
+    },
+    {
+      path: 'page-500',
+      element: <Home />,
+      meta: {
+        title: '500页面'
+      }
+    }
+  ]
+}
+
+export default ExceptionRoute
