665e26670d1d6421e82b8ce93f0dbf0c1d63a405
baimingxuan <354372738@qq.com>
2023-04-18 14:04:43 +0800

    feat(router): Create form route

---
+++ src/router/routes/form.tsx
@@ -0,0 +1,31 @@
+import { AppRoute } from '../types'
+import { BasicLayout } from '../../layout'
+import Home from '../../views/home'
+
+// form module page
+const FormRoute: AppRoute = {
+  path: '/form',
+  element: <BasicLayout />,
+  meta: {
+      title: '表单',
+      icon: 'form'
+  },
+  children: [
+      {
+          path: 'form-list',
+          element: <Home />,
+          meta: {
+              title: '表单列表'
+          }
+      },
+      {
+          path: 'form-marking',
+          element: <Home />,
+          meta: {
+              title: '表单生成器'
+          }
+      }
+  ]
+}
+
+export default FormRoute
