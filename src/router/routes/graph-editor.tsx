4a092a54ad72ab42f715dd22887b68d64095d087
baimingxuan <354372738@qq.com>
2023-04-28 17:42:19 +0800

    feat(router): Create graph-editor route

---
+++ src/router/routes/graph-editor.tsx
@@ -0,0 +1,31 @@
+import { AppRoute } from '../types'
+import { BasicLayout } from '../../layout'
+import Home from '../../views/home'
+
+// graph-editor module page
+const GraphEditorRoute: AppRoute = {
+  path: '/graph-editor',
+  element: <BasicLayout />,
+  meta: {
+      title: '图形编辑器',
+      icon: 'flow'
+  },
+  children: [
+      {
+          path: 'flow-chart',
+          element: <Home />,
+          meta: {
+              title: '流程图'
+          }
+      },
+      {
+          path: 'mind',
+          element: <Home />,
+          meta: {
+              title: '思维导图'
+          }
+      }
+  ]
+}
+
+export default GraphEditorRoute
