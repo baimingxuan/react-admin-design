ce8a1d49dfbbdc8ed9d77d6f4a3cd357357d4e6e
baimingxuan <354372738@qq.com>
2023-05-04 18:16:21 +0800

    feat(router): Create images route

---
+++ src/router/routes/images.tsx
@@ -0,0 +1,38 @@
+import { AppRoute } from '../types'
+import { BasicLayout } from '../../layout'
+import Home from '../../views/home'
+
+// image module page
+const ImageRoute: AppRoute = {
+    path: '/image',
+    element: <BasicLayout />,
+    meta: {
+        title: '图片处理',
+        icon: 'image'
+    },
+    children: [
+        {
+            path: 'image-cropper',
+            element: <Home />,
+            meta: {
+                title: '图片裁剪'
+            }
+        },
+        {
+            path: 'image-compress',
+            element: <Home />,
+            meta: {
+                title: '图片压缩'
+            }
+        },
+        {
+            path: 'image-composition',
+            element: <Home />,
+            meta: {
+                title: '图片合成'
+            }
+        }
+    ]
+}
+
+export default ImageRoute
