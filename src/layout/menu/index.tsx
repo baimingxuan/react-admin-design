c0966c1fe947a2041d3cc229a5c0f9ab1d65b9b6
baimingxuan <354372738@qq.com>
2023-05-11 20:31:37 +0800

    feat(layout): Update LayoutMenu compo

--- src/layout/menu/index.tsx
+++ src/layout/menu/index.tsx
@@ -1,8 +1,28 @@
+import type { MenuProps } from 'antd'
 import React, { useState } from 'react'
 import { Menu, Spin } from 'antd'
 
+type MenuItem = Required<MenuProps>['items'][number]
+
 const LayoutMenu = (props: any) => {
   const [loading, setLoading] = useState(false)
+  const [menuList, setMenuList] = useState<MenuItem[]>([])
+
+  const getItem = (
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+    type?: 'group'
+  ): MenuItem => {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      type
+    } as MenuItem
+  }
 
   return (
     <div className='layout_menu'>
@@ -15,3 +35,5 @@ const LayoutMenu = (props: any) => {
     </div>
   )
 }
+
+export default LayoutMenu
