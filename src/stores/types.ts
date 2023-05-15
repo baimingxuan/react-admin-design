cc0616e33cbb886b95ddf53a5b9e8a01805d2c32
baimingxuan <354372738@qq.com>
2023-05-15 22:07:09 +0800

    feat(redux): Create type for redux

---
+++ src/stores/types.ts
@@ -0,0 +1,19 @@
+interface MenuOptions {
+  path: string
+  title: string
+  icon?: string
+  isLink?: string
+  close?: boolean
+  children?: MenuOptions[]
+}
+
+export interface MenuState {
+  isCollapse: boolean
+  menuList: MenuOptions[]
+}
+
+export interface BreadcrumbState {
+  breadcrumbList: {
+    [propName: string]: any
+  }
+}
