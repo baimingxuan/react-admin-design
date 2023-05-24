8f785066ae31388a588def993c7225d4af7ac8b4
baimingxuan <354372738@qq.com>
2023-05-24 18:13:50 +0800

    feat(utils): Add treeHelper to utils

--- src/router/helpers.ts
+++ src/router/helpers.ts
@@ -1,8 +1,9 @@
-import type { RouteObject } from './types'
+import type { RouteObject, AppMenu } from './types'
 import { cloneDeep } from 'lodash-es'
 import { isUrl } from '@/utils/is'
+import { treeMap } from '@/utils/helper/treeHelper'
 
-function joinParentPath(menus: any[], parentPath = '') {
+function joinParentPath(menus: AppMenu[], parentPath = '') {
   for (let index = 0; index < menus.length; index++) {
     const menu = menus[index]
     // Note that nested paths that start with / will be treated as a root path.
@@ -20,5 +21,19 @@ function joinParentPath(menus: any[], parentPath = '') {
 export function transformRouteToMenu(routes: RouteObject[]) {
   const cloneRoutes = cloneDeep(routes)
 
+  const list = treeMap(cloneRoutes, {
+    conversion: (node: RouteObject) => {
+      const { meta: { title, hideMenu = false, ...rest } = {} } = node
 
+      return {
+        ...(rest || {}),
+        name: title,
+        hideMenu,
+        path: node.path
+      }
+    }
+  }) as AppMenu[]
+
+  joinParentPath(list)
+  return cloneDeep(list)
 }
