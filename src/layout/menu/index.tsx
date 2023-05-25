68f73cbefd1f4612ba4caf11fd713a0261ca161d
baimingxuan <354372738@qq.com>
2023-05-25 22:58:53 +0800

    feat(menu): Update layout menu compo

--- src/layout/menu/index.tsx
+++ src/layout/menu/index.tsx
@@ -2,7 +2,9 @@ import type { MenuProps } from 'antd'
 import React, { useState, useEffect } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { Menu, Spin } from 'antd'
+import * as Icons from '@ant-design/icons'
 import { getAsyncMenus } from '@/router/menus'
+import { AppMenu } from '@/router/types'
 
 type MenuItem = Required<MenuProps>['items'][number]
 
@@ -33,11 +35,27 @@ const LayoutMenu = (props: any) => {
     setSelectedKeys([pathname])
   }, [pathname])
 
+  const customIcons: { [key: string]: any } = Icons
+  const addIcon = (name: string) => {
+    return React.createElement(customIcons[name])
+  }
+
+  const getMenuItem = (menuData: AppMenu[], menuList: MenuItem[] = []) => {
+    menuData.forEach((item: AppMenu) => {
+      if (!item?.children?.length) {
+        return menuList.push(getItem(item.name, item.path, addIcon(item.icon!)))
+      }
+      menuList.push(getItem(item.name, item.path, addIcon(item.icon!), getMenuItem(item.children)))
+    })
+    return menuList
+  }
+
   const getMenuList = async () => {
     setLoading(true)
     try {
       const menus = await getAsyncMenus()
       console.log('menus', menus)
+      setMenuList(getMenuItem(menus))
     } finally {
       setLoading(false)
     }
