e41a2c4586407c5540590062477fba40baa59d4b
baimingxuan <354372738@qq.com>
2023-06-03 10:46:07 +0800

    feat(compo): Add SvgIcon compo to components

---
+++ src/components/SvgIcon/index.tsx
@@ -0,0 +1,20 @@
+import { SvgIconProp } from './types'
+import styles from './index.module.less'
+
+export default function SvgIcon({
+  name,
+  prefix = 'icon',
+  size = 16
+} : SvgIconProp) {
+  const symbolId = `#${prefix}-${name}`
+  const iconStyle = {
+    width: `${size}px`,
+    height: `${size}px`
+  }
+
+  return (
+    <svg className={styles['svg-icon']} style={iconStyle} aria-hidden="true">
+      <use href={symbolId} />
+    </svg>
+  )
+}
---
+++ src/components/SvgIcon/types.ts
@@ -0,0 +1,5 @@
+export interface SvgIconProp {
+  name: string
+  prefix?: string
+  size?: number
+}
