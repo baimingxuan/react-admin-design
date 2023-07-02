2a7257e24bffde633e74dcfe51189819dfbff3ff
baimingxuan <354372738@qq.com>
2023-07-02 16:36:05 +0800

    feat: Add GithubLink compo

---
+++ src/layout/feature/components/GithubLink.tsx
@@ -0,0 +1,27 @@
+import { Tooltip } from 'antd'
+import { openWindow } from '@/utils'
+import SvgIcon from '@/components/SvgIcon'
+
+export default function GithubLink() {
+  function openGithub () {
+    openWindow('https://github.com/baimingxuan/vue3-admin-design')
+  }
+
+  return (
+    <Tooltip
+      title='github'
+      placement='bottom'
+      mouseEnterDelay={0.5}
+    >
+      <span
+        className='icon-btn'
+        onClick={openGithub}
+      >
+        <SvgIcon
+          name='github'
+          size={20}
+        />
+      </span>
+    </Tooltip>
+  )
+}
