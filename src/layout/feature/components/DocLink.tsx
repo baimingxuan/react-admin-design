171a72a60050d314e5f2948bfb1afe1efc82933d
baimingxuan <354372738@qq.com>
2023-07-02 14:54:00 +0800

    feat: Add Doclink compo to layoutFeature

---
+++ src/layout/feature/components/DocLink.tsx
@@ -0,0 +1,16 @@
+import { Tooltip } from 'antd'
+import SvgIcon from '@/components/SvgIcon'
+
+export default function() {
+  return (
+    <Tooltip
+      title='文档'
+      placement='bottom'
+      mouseEnterDelay={0.5}
+    >
+      <span className='icon-btn'>
+        <SvgIcon name='document' size={20} />
+      </span>
+    </Tooltip>
+  )
+}
