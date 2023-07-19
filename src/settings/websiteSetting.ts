673b23ba7d9a69c785357f78346cf0ba563ac0bc
baimingxuan <354372738@qq.com>
2023-07-19 22:41:59 +0800

    feat: Create settings module and add websiteSetting file

---
+++ src/settings/websiteSetting.ts
@@ -0,0 +1,151 @@
+// Github repo url
+export const GITHUB_URL = 'https://github.com/baimingxuan/vue3-admin-design'
+
+// Form component
+export const FORM_COMPO = {
+  name: 'Form表单',
+  url: 'https://www.antdv.com/components/form',
+  desc: 'ant-design-form: 使用 ant-design 的 form 组件, 可用以收集、校验和提交数据等操作。'
+}
+
+// Form-create plugin
+export const FORM_CREATE_DESIGNER = {
+  name: 'Form表单设计器',
+  url: 'https://github.com/xaboy/form-create-designer/tree/next',
+  desc: 'form-create-designer: 一个通过拖拽的方式快速创建表单的设计器组件, 能提高开发者对表单的开发效率, 节省开发者的时间。'
+}
+
+// Table component
+export const TABLE_COMPO = {
+  name: 'Table表格',
+  url: 'https://www.antdv.com/components/table',
+  desc: 'ant-design-table: 使用 ant-design 的 table 组件, 可用于展示多条结构类似的数据, 并对其进行相关操作。'
+}
+
+// Table edit component
+export const TABLE_EDIT_COMPO = {
+  name: 'Table表格(可编辑行)',
+  url: 'https://www.antdv.com/components/table',
+  desc: 'ant-design-table: 使用 ant-design 的 table 组件, 可用于展示多条结构类似的数据, 并对其进行行数据编辑操作。'
+}
+
+// Tree component
+export const TREE_COMPO = {
+  name: 'Tree树形控件',
+  url: 'https://www.antdv.com/components/tree',
+  desc: 'ant-design-tree: 基于Ant-Design的Tree组件, 可以完整展现其中的层级关系, 并具有展开收起选择等交互功能。'
+}
+
+// Transfer component
+export const TRANSFER_COMPO = {
+  name: 'Transfer穿梭框',
+  url: 'https://www.antdv.com/components/transfer',
+  desc: 'ant-design-transfer: 使用 ant-design 的 transfer 组件, 可用于对列表数据进行选中、取消等操作。'
+}
+
+// Upload component
+export const UPLOAD_COMPO = {
+  name: 'Upload图片上传组件',
+  url: 'https://antdv.com/components/upload',
+  desc: 'ant-design-upload: 使用 ant-design 的 upload 组件, 并具有多种列表展示方式。'
+}
+
+// Video player plugin
+export const VIDEO_PLUGIN = {
+  name: 'Video视频播放器',
+  url: 'https://github.com/surmon-china/videojs-player',
+  desc: 'videojs-player: 一个支持 vue3 的 video.js 视频播放器组件, 适用于目前主流网络视频的播放。'
+}
+
+// Video resource src
+export const VIDEO_RES_SRC = 'https://cdn.jsdelivr.net/gh/baimingxuan/media-store/videos/houlang.mp4'
+
+// Xlsx plugin
+export const XLSX_PLUGIN = {
+  name: 'JS-xlsx插件',
+  url: 'https://github.com/SheetJS/sheetjs',
+  desc: 'JS-xlsx: 由SheetJS出品的一款非常方便的只需要纯JS即可读取和导出excel的工具库, 功能强大, 支持xlsx、csv、txt等格式。'
+}
+
+// Vue-Draggable plugin
+export const DRAGGABLE_PLUGIN = {
+  name: 'Draggable拖拽列表',
+  url: 'https://github.com/SortableJS/vue.draggable.next',
+  desc: 'Vue.Draggable: 基于Sortable.js的vue组件, 用以实现拖拽功能。'
+}
+
+// Vue-drag-resize plugin
+export const VUE_DRAG_RESIZE_PLUGIN = {
+  name: 'DragResize拖拽组件',
+  url: 'https://github.com/kirillmurashov/vue-drag-resize/tree/vue3',
+  desc: 'vue-drag-resize: 一款用于可拖动和调整元素大小的 vue3 组件。'
+}
+
+// Vue-cropper plugin
+export const VUE_CROPPER_PLUGIN = {
+  name: 'Vue-Cropper图片裁剪',
+  url: 'https://github.com/xyxiao001/vue-cropper',
+  desc: 'vue-cropper: 一个优雅的图片裁剪插件, 可实现图片裁剪、图片生成等功能, 并支持生成png、jpeg、webp等图片格式。'
+}
+
+// Image-composition component
+export const IMAGE_COMPOSITION = {
+  name: 'Image图片合成',
+  url: 'https://github.com/xyxiao001/vue-cropper',
+  desc: '图片合成: 基于VueDRR拖拽功能, 在其上通过叠加图片、文字等, 实现图片的叠加伪合成功能。'
+}
+
+// Cropper image src
+export const CROPPER_IMG_SRC = 'https://cdn.jsdelivr.net/gh/baimingxuan/media-store/images/img02.jpg'
+
+// Compress image src
+export const COMPRESS_IMG_SRC = 'https://cdn.jsdelivr.net/gh/baimingxuan/media-store/images/img03.jpg'
+
+// Image-compress component
+export const IMAGE_COMPRESS = {
+  name: 'Image图片压缩',
+  url: 'https://github.com/baimingxuan/vue3-admin-design/blob/main/src/views/image/image-compress.tsx',
+  desc: 'ImageCompress: 纯JS实现对图片的等比压缩和放大的功能, 并能对图片进行下载。'
+}
+
+// Count-to plugin
+export const COUNTTO_PLUGIN = {
+  name: 'CountTo数字滚动',
+  url: 'https://github.com/baimingxuan/vue3-admin-design/blob/main/src/components/CountTo/src/index.tsx',
+  desc: 'CountTo: 一个无依赖、轻量级的vue3数字滚动插件, 可以通过你自己的方式轻松编写。'
+}
+
+// Vue-codemirror plugin
+export const CODEMIRROR_PLUGIN = {
+  name: 'CodeMirror代码编辑器',
+  url: 'https://github.com/surmon-china/vue-codemirror',
+  desc: 'VueCodeMirror: 是一款基于vue的代码编辑器, 可支持html、javascript、typescript等。'
+}
+
+// Antv-g6 plugin
+export const ANTV_G6_PLUGIN = {
+  name: 'AntV-G6流程图',
+  url: 'https://github.com/antvis/g6',
+  desc: 'antv-g6: 一个图可视化引擎。它提供了图的绘制、布局、分析、交互、动画等图可视化的基础能力。'
+}
+
+// VueTreeOrg plugin
+export const VUE_TREE_ORG_PLUGIN = {
+  name: 'Tree-Org树状组织图',
+  url: 'https://github.com/sangtian152/vue3-tree-org',
+  desc: 'vue3-tree-org: 基于vue的树结构组织图, 可用于公司组织架构展示。'
+}
+
+// WangEditor plugin
+export const WANG_EDITOR_PLUGIN = {
+  name: '富文本编辑器',
+  url: 'https://github.com/wangeditor-team/wangEditor',
+  desc: 'wangEditor: 基于javascript和css开发的 Web富文本编辑器, 轻量、简洁、易用、开源免费。'
+}
+
+// Vue-markdown-editor plugin
+export const MARKDOWN_EDITOR_PLUGIN = {
+  name: 'Markdown编辑器',
+  url: 'https://github.com/code-farmer-i/vue-markdown-editor',
+  desc: 'VMdEditor: 是一款基于vue的Markdown编辑器,比较适合博客系统。'
+}
