58f11927cfe19fd21a636d89e7a185c2cf6c433c
baimingxuan <354372738@qq.com>
2023-07-24 22:21:25 +0800

    feat: Add axios plugin and encapsulate axios function

---
+++ src/utils/axios.ts
@@ -0,0 +1,46 @@
+import type { InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
+import axios from 'axios'
+import { message } from 'antd'
+
+// Create axios instance
+const service = axios.create({
+  baseURL: '/api',
+  timeout: 10 * 1000
+})
+
+// Handle Error
+const handleError = (error: AxiosError): Promise<AxiosError> => {
+  if (error.response?.status === 401 || error.response?.status === 504) {
+    // clearAuthCache()
+    // router.push({ path: '/login' })
+  }
+  message.error(error.message || 'error')
+  return Promise.reject(error)
+}
+
+// Request interceptors configuration
+service.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const token = ''
+  if (token) {
+    (config as Recordable).headers['Authorization'] = `${token}`
+  }
+  (config as Recordable).headers['Content-Type'] = 'application/json'
+  return config
+},
+handleError)
+
+// Respose interceptors configuration
+service.interceptors.response.use((response: AxiosResponse) => {
+  const data = response.data
+
+  if (data.code === 0) {
+    return data.data
+  } else {
+    message.error(data.message)
+
+    return Promise.reject('error')
+  }
+},
+handleError)
+
+export { service }
