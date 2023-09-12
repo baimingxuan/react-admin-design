79c87dacc89b03f98327314e2b54941282ce73ea
baimingxuan <354372738@qq.com>
2023-09-13 00:05:59 +0800

    feat: Add react-countup plugin to CountTo page

--- src/views/compo/count-to.tsx
+++ src/views/compo/count-to.tsx
@@ -1,14 +1,24 @@
 import React, { useRef, useState } from 'react'
 import { Row, Col, Card, Button, Form, Input, InputNumber, Space } from 'antd'
 import { PageWrapper } from '@/components/Page'
-import { CountTo } from '@/components/CountTo'
+import CountUp from 'react-countup'
 import { COUNTTO_PLUGIN } from '@/settings/websiteSetting'
 
 const CountToPage: React.FC = () => {
-  const countRef = useRef(null)
+  const [form] = Form.useForm()
+  const [formData, setFromData] = useState({
+    startVal: 0,
+    endVal: 2020,
+    duration: 4,
+    decimals: 0,
+    separator: ',',
+    prefix: '￥ ',
+    suffix: ' rmb'
+  })
 
-  const handleStart = () => {
-    // countRef.current?.start()
+  const onFinish = (values: any) => {
+    setFromData({...formData, ...values})
+    console.log('values', values)
   }
 
   const handleReset = () => {
@@ -20,13 +30,14 @@ const CountToPage: React.FC = () => {
       <Row gutter={12}>
         <Col span={6}>
           <Card title='正向增加' bordered={false} bodyStyle={{height: '300px'}}>
-            <CountTo
-              startVal={0}
-              endVal={2020}
-              duration={4000}
-              size={40}
+            <CountUp
+              start={0}
+              end={2020}
+              duration={4}
               style={{
-                height: '100%'
+                height: '100%',
+                fontSize: '40px',
+                color: '#e65d6e'
               }}
               className='flex-center'
             />
@@ -34,29 +45,33 @@ const CountToPage: React.FC = () => {
         </Col>
         <Col span={12}>
           <Card title='自定义配置' bordered={false} bodyStyle={{height: '300px'}}>
-            {/* <div className='flex-center' style={{marginBottom: '30px'}}>
-              <CountTo
-                ref={countRef}
-                startVal={formRef.startVal}
-                endVal={formRef.endVal}
-                duration={formRef.duration}
-                decimals={formRef.decimals}
-                separator={formRef.separator}
-                prefix={formRef.prefix}
-                suffix={formRef.suffix}
-                size={40}
-                autoplay={false}
+            <div className='flex-center' style={{marginBottom: '30px'}}>
+              <CountUp
+                // ref={countRef}
+                start={formData.startVal}
+                end={formData.endVal}
+                duration={formData.duration}
+                decimals={formData.decimals}
+                separator={formData.separator}
+                prefix={formData.prefix}
+                suffix={formData.suffix}
+                style={{
+                  fontSize: '40px',
+                  color: '#e65d6e'
+                }}
+                onReset={handleReset}
               />
             </div>
             <Form
-              model={formRef}
+              form={form}
+              initialValues={{...formData}}
               layout='inline'
               labelAlign='left'
               labelCol={{style: {width: '80px', marginBottom: '12px'}}}
+              onFinish={onFinish}
             >
               <Form.Item label='startVal:' name='startVal'>
                 <InputNumber
-                  v-model:value={formRef.startVal}
                   min={0}
                   max={10000}
                   style={{width: '100px'}}
@@ -64,7 +79,6 @@ const CountToPage: React.FC = () => {
               </Form.Item>
               <Form.Item label='endVal:' name='endVal'>
                 <InputNumber
-                  v-model:value={formRef.endVal}
                   min={0}
                   max={10000}
                   style={{width: '100px'}}
@@ -72,47 +86,47 @@ const CountToPage: React.FC = () => {
               </Form.Item>
               <Form.Item label='duration:' name='duration'>
                 <InputNumber
-                  v-model:value={formRef.duration}
-                  min={100}
-                  max={100000}
+                  min={1}
+                  max={100}
                   style={{width: '100px'}}
                 />
               </Form.Item>
               <Form.Item label='decimals:' name='decimals'>
                 <InputNumber
-                  v-model:value={formRef.decimals}
                   min={0}
                   max={100}
                   style={{width: '100px'}}
                 />
               </Form.Item>
               <Form.Item label='separator:' name='separator'>
-                <Input v-model:value={formRef.separator} style={{width: '100px'}} />
+                <Input style={{width: '100px'}} />
               </Form.Item>
               <Form.Item label='prefix:' name='prefix'>
-                <Input v-model:value={formRef.prefix} style={{width: '100px'}} />
+                <Input style={{width: '100px'}} />
               </Form.Item>
               <Form.Item label='suffix:' name='suffix'>
-                <Input v-model:value={formRef.suffix} style={{width: '100px'}} />
+                <Input style={{width: '100px'}} />
               </Form.Item>
               <Form.Item>
                 <Space>
-                  <Button type='primary' onClick={handleStart}>开始</Button>
+                  <Button type='primary' htmlType="submit">开始</Button>
                   <Button type='primary' danger onClick={handleReset}>重置</Button>
                 </Space>
               </Form.Item>
-            </Form> */}
+            </Form>
           </Card>
         </Col>
         <Col span={6}>
           <Card title='反向减少' bordered={false} bodyStyle={{height: '300px'}}>
-            <CountTo
-              startVal={2020}
-              endVal={0}
-              duration={4000}
-              size={40}
-              color='#30b08f'
-              style={{height: '100%'}}
+            <CountUp
+              start={2020}
+              end={0}
+              duration={4}
+              style={{
+                height: '100%',
+                fontSize: '40px',
+                color: '#30b08f'
+              }}
               className='flex-center'
             />
           </Card>
