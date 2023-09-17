af12d22fcbb58f6f8be268b8c01839656a09c547
baimingxuan <354372738@qq.com>
2023-09-17 15:05:18 +0800

    feat: Create Tooltip and Editor component for rich-text page

---
+++ src/views/editor/rich-text/components/Toolbar.tsx
@@ -0,0 +1,31 @@
+import { useRef, useEffect } from 'react'
+import * as wangEditor from '@wangeditor/editor'
+
+interface IProps {
+  editor: wangEditor.IDomEditor | null
+  defaultConfig?: Partial<wangEditor.IToolbarConfig>
+  mode?: string
+  style?: object
+  className?: string
+}
+
+function ToolbarComponent(props: IProps) {
+  const { editor, defaultConfig = {}, mode = 'default', style = {}, className } = props
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (ref.current == null) return
+    if (editor == null) return
+
+    wangEditor.createToolbar({
+      editor,
+      selector: ref.current,
+      config: defaultConfig,
+      mode,
+    })
+  }, [editor])
+
+  return <div style={style} ref={ref} className={className}></div>
+}
+
+export default ToolbarComponent
