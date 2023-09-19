a28f8790e2113cbcccfe07ace94286eb04c20c92
baimingxuan <354372738@qq.com>
2023-09-19 10:43:33 +0800

    feat: Create form-create page

---
+++ src/views/form/form-create.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+import { Card } from 'antd'
+import { PageWrapper } from '@/components/Page'
+import { FORM_CREATE_DESIGNER } from '@/settings/websiteSetting'
+
+const FormCreate: FC = () => {
+
+  return (
+    <PageWrapper plugin={FORM_CREATE_DESIGNER}>
+      <Card bordered={false}></Card>
+    </PageWrapper>
+  )
+}
+
+export default FormCreate
