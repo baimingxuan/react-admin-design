1aa147a3b658278487ab7c5671406c8d9d3c20a0
baimingxuan <354372738@qq.com>
2023-10-06 23:06:14 +0800

    feat: Update home page

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,11 +1,45 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import { Row, Col, Space } from 'antd'
+import { countUpData } from './data'
+import CountUpCard from './components/CountUpCard'
+import ChartsBar from './components/ChartsBar'
 
 const HomePage: FC = () => {
+	const [isLoading, setIsLoading] = useState(true)
+
+	setTimeout(() => {
+		setIsLoading(false)
+	}, 1500)
+
 	return (
-		<div className="home card">
-			首页
-		</div>
-	);
-};
+		<Space direction='vertical' size={12} style={{ display: 'flex' }}>
+			<Row gutter={12}>
+				{
+					countUpData.map(item => {
+						return (
+							<Col flex={1} key={item.title}>
+								<CountUpCard
+									loading={isLoading}
+									title={item.title}
+									color={item.color}
+									iconName={item.icon}
+									countNum={item.count}
+								/>
+							</Col>
+						)
+					})
+				}
+			</Row>
+			<Row gutter={12}>
+          <Col span={12}>
+            <ChartsBar loading={isLoading} />
+          </Col>
+          <Col span={12}>
+            {/* <ChartsLine loading={unref(isLoading)} /> */}
+          </Col>
+        </Row>
+		</Space>
+	)
+}
 
 export default HomePage
