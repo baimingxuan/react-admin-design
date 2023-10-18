a02a858f97fef9bc6a4695be0d90dfb04c6bf7a0
baimingxuan <354372738@qq.com>
2023-10-18 09:17:49 +0800

    feat: Add prettierrc.cjs file

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  // 每行最大列，超过换行
+  // printWidth: 120,
+  // 使用制表符而不是空格缩进
+  useTabs: false,
+  // 缩进
+  tabWidth: 2,
+  // 结尾不用分号
+  semi: false,
+  // 使用单引号
+  singleQuote: true,
+  // 在JSX中使用单引号而不是双引号
+  jsxSingleQuote: true,
+  // 箭头函数里面，如果是一个参数的时候，去掉括号
+  arrowParens: 'avoid',
+  // 对象、数组括号与文字间添加空格
+  bracketSpacing: true,
+  // 尾随逗号
+  trailingComma: 'none',
+}
