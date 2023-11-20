934c8db01ddcda7cc970ca010b8db496beb7b9fe
baimingxuan <354372738@qq.com>
2023-11-21 00:36:50 +0800

    chore: 更新eslint配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,20 @@
-// @see: https://eslint.nodejs.cn/
+// 配置文档: https://eslint.nodejs.cn/
 
 module.exports = {
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
   root: true,
   env: {
     browser: true,
     node: true,
     es6: true
   },
-  /* 指定如何解析语法 */
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  // 指定如何解析语法
   parser: '@typescript-eslint/parser',
-  /* 优先级低于 parse 的语法解析配置 */
+  // 优先级低于 parse 的语法解析配置
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
@@ -24,7 +24,7 @@ module.exports = {
     }
   },
   plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
-  /* 继承某些已有的规则 */
+  // 继承某些已有的规则
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -35,19 +35,38 @@ module.exports = {
     'plugin:prettier/recommended'
   ],
   /*
-   * "off" 或 0    ==>  关闭规则
-   * "warn" 或 1   ==>  打开的规则作为警告（不影响代码执行）
-   * "error" 或 2  ==>  规则作为一个错误（代码不能执行，界面报错）
+   * 'off' 或 0    ==>  关闭规则
+   * 'warn' 或 1   ==>  规则提示为警告（不影响代码执行）
+   * 'error' 或 2  ==>  规则提示为错误（代码不能执行，界面报错）
    */
   rules: {
-    // eslint (http://eslint.cn/docs/rules)
+    // eslint (https://eslint.nodejs.cn/docs/latest/rules/)
     'no-var': 'error', // 要求使用 let 或 const 而不是 var
-    'no-multiple-empty-lines': ['error', { max: 1 }], // 不允许多个空行
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1
+      }
+    ], // 不允许多个空行
     'no-use-before-define': 'off', // 禁止在 函数/类/变量 定义之前使用它们
     'prefer-const': 'off', // 此规则旨在标记使用 let 关键字声明但在初始分配后从未重新分配的变量，要求使用 const
     'no-irregular-whitespace': 'off', // 禁止不规则的空白
+    'no-debugger': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ],
 
-    // typeScript (https://typescript-eslint.io/rules)
+    // typeScript (https://typescript-eslint.nodejs.cn/rules/)
     '@typescript-eslint/no-unused-vars': 'error', // 禁止定义未使用的变量
     '@typescript-eslint/no-inferrable-types': 'off', // 可以轻松推断的显式类型可能会增加不必要的冗长
     '@typescript-eslint/no-namespace': 'off', // 禁止使用自定义 TypeScript 模块和命名空间。
@@ -61,6 +80,27 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off', // 禁止 @ts-<directive> 使用注释或要求在指令后进行描述
     '@typescript-eslint/no-non-null-assertion': 'off', // 不允许使用后缀运算符的非空断言(!)
     '@typescript-eslint/explicit-module-boundary-types': 'off', // 要求导出函数和类的公共类方法的显式返回和参数类型
+    '@typescript-eslint/no-import-type-side-effects': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        disallowTypeAnnotations: false,
+        fixStyle: 'inline-type-imports'
+      }
+    ],
+    '@typescript-eslint/prefer-literal-enum-member': [
+      'error',
+      {
+        allowBitwiseExpressions: true
+      }
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }
+    ],
 
     // react (https://github.com/jsx-eslint/eslint-plugin-react)
     'react-hooks/rules-of-hooks': 'off',
