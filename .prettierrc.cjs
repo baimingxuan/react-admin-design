f680a67fbcc7ea7ad78d762bdf8c4c83ceb27537
baimingxuan <354372738@qq.com>
2023-11-22 18:06:43 +0800

    chore: 更新prettier配置

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,24 +1,26 @@
 // 配置文档: https://prettier.nodejs.cn/
 
 module.exports = {
-  // 每行最大列，超过换行
+  // 每行最大宽度，超过换行
   printWidth: 120,
-  // 使用制表符而不是空格缩进
-  useTabs: false,
-  // 缩进字节数
+  // 缩进级别的空格数
   tabWidth: 2,
-  // 结尾不用分号(true有，false没有)
+  // 用制表符而不是空格缩进行
+  useTabs: false,
+  // 语句末尾用分号
   semi: false,
-  // 使用单引号(true单引号，false双引号)
+  // 使用单引号而不是双引号
   singleQuote: true,
-  // 在JSX中使用单引号而不是双引号
+  // 在 JSX 中使用单引号而不是双引号
   jsxSingleQuote: true,
-  // 在对象、数组括号与文字之间加空格 "{ foo: bar }"
-  bracketSpacing: true,
-  // 箭头函数里面，如果是一个参数的时候，去掉括号(avoid：省略括号, always：不省略括号)
-  arrowParens: 'avoid',
   // 尾随逗号
   trailingComma: 'none',
-  // 换行符使用 lf 结尾是 可选值"<auto|lf|crlf|cr>"
+  // 对象字面量中括号之间有空格 { foo: bar }
+  bracketSpacing: true,
+  // 将多行 HTML（HTML、JSX）元素的 > 放在最后一行的末尾，而不是单独放在下一行
+  bracketSameLine: false,
+  // 在唯一的箭头函数参数周围包含括号(avoid：省略括号, always：不省略括号)
+  arrowParens: 'avoid',
+  // 换行符使用 lf 结尾 可选值 auto|lf|crlf|cr
   endOfLine: 'lf'
 }
