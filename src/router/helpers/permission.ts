0526aa68cff171af67d692c4fa6dba0c2e2edec0
baimingxuan <354372738@qq.com>
2023-11-28 18:34:37 +0800

    chore: 更新部分插件

--- src/router/helpers/permission.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'react-router-dom'
-import { getToken } from '@/utils/auth'
-
-export const permissionLoader = () => {
-  if (!getToken()) return redirect('/login')
-  return {}
-}
