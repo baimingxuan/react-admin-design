5cfffed701631e275585a3ae3638d008874747f0
baimingxuan <354372738@qq.com>
2023-12-03 23:28:51 +0800

    feat: 完成菜单折叠功能

--- src/layout/header/components/FoldTrigger/index.tsx
+++ src/layout/header/components/FoldTrigger/index.tsx
@@ -1,16 +1,18 @@
+import { useAppSelector, useAppDispatch } from '@/stores'
+import { setAppConfig } from '@/stores/modules/appSlice'
 import SvgIcon from '@/components/SvgIcon'
 import style from './index.module.less'
 
 export default function FoldTrigger() {
-  function toggledMenuFold() {}
-  
+  const getMenuFold = useAppSelector(state => state.app.appConfig?.menuSetting?.menuFold)
+  const dispatch = useAppDispatch()
+
+  function toggledMenuFold() {
+    dispatch(setAppConfig({ menuSetting: { menuFold: !getMenuFold } }))
+  }
+
   return (
-    <span
-      className={
-        style['compo_fold-trigger']
-      }
-      onClick={toggledMenuFold}
-    >
+    <span className={style['compo_fold-trigger']} onClick={toggledMenuFold}>
       <SvgIcon name='unfold' size={20} />
     </span>
   )
