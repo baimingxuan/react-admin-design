1d3607d3e2d1b013a99139785198c490f452def6
baimingxuan <354372738@qq.com>
2023-12-12 09:49:10 +0800

    fix: 修复页面标签出现undefined的问题

--- src/hooks/web/useTitle.ts
+++ src/hooks/web/useTitle.ts
@@ -2,17 +2,16 @@ import { useState, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import { useTitle as usePageTitle } from 'ahooks'
 import { searchRoute } from '@/utils'
-import { useAppSelector } from '@/stores'
+import { basicRoutes } from '@/router'
 
 // 监听页面变化和动态改变网站标题
 export function useTitle() {
   const [pageTitle, setPageTitle] = useState('react-admin-design')
   const { pathname } = useLocation()
-  const { menuList } = useAppSelector(state => state.menu)
 
   useEffect(() => {
-    const currRoute = searchRoute(pathname, menuList)
-    setPageTitle(currRoute?.name!)
+    const currRoute = searchRoute(pathname, basicRoutes)
+    setPageTitle(currRoute?.meta.title)
   }, [pathname])
 
   usePageTitle(pageTitle)
