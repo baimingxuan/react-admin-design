f9068e7526c127518caebbbabca7336cfc506feb
baimingxuan <354372738@qq.com>
2023-12-15 18:22:29 +0800

    perf: 更换视频播放组件

--- src/views/video/video-player.tsx
+++ src/views/video/video-player.tsx
@@ -1,42 +1,31 @@
-
 // @ts-nocheck
 import { Row, Col, Card } from 'antd'
 import { PageWrapper } from '@/components/Page'
 import { VIDEO_RES_SRC, VIDEO_PLUGIN } from '@/settings/websiteSetting'
-import { Player, ControlBar, BigPlayButton, CurrentTimeDisplay, TimeDivider,
-  PlaybackRateMenuButton, VolumeMenuButton } from 'video-react'
-import 'video-react/dist/video-react.css'
+import { VideoPlayer } from '@/components/VideoPlayer'
 
-const VideoPlayers = () => {
-
+const VideoPlayer = () => {
   return (
     <PageWrapper plugin={VIDEO_PLUGIN}>
       <Row gutter={12}>
         <Col span={12}>
           <Card title='传统视频播放器' bordered={false}>
-            <video 
-              src={VIDEO_RES_SRC}
-              controls
-              style={{width: '100%', outline: 'none'}}
-            />
+            <video src={VIDEO_RES_SRC} controls style={{ width: '100%', outline: 'none' }} />
           </Card>
         </Col>
         <Col span={12}>
           <Card title='视频播放插件' bordered={false}>
-            <Player
-              src={VIDEO_RES_SRC}
-              fluid
-              preload='auto'
-              aspectRatio='16:9'
-            >
-              <BigPlayButton position="center" />
-              <ControlBar>
-                <CurrentTimeDisplay order={4.1} />
-                <TimeDivider order={4.2} />
-                <VolumeMenuButton vertical order={7.0} />
-                <PlaybackRateMenuButton rates={[0.5, 1.0, 1.5, 2.0]} order={7.2} />
-              </ControlBar>
-            </Player>
+            <VideoPlayer
+              options={{
+                sources: [{ src: VIDEO_RES_SRC, type: 'video/mp4' }],
+                playbackRates: [0.5, 1.0, 1.5, 2.0],
+                controls: true,
+                fluid: true,
+                loop: false,
+                preload: 'auto',
+                aspectRatio: '16:9'
+              }}
+            />
           </Card>
         </Col>
       </Row>
@@ -44,4 +33,4 @@ const VideoPlayers = () => {
   )
 }
 
-export default VideoPlayers
+export default VideoPlayer
