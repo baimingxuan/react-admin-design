c1d1dd5a8155f246ee784cb9c3a1cd576b6229d3
baimingxuan <354372738@qq.com>
2023-12-25 18:19:15 +0800

    feat: 创建 RichTextSetting 组件

---
+++ src/components/RichText/src/RichTextSetting.tsx
@@ -0,0 +1,94 @@
+import type { FC } from 'react'
+import type { MenuProps } from 'antd'
+import { useState } from 'react'
+import { Form, Button, Space, Select, ColorPicker, Dropdown } from 'antd'
+import RichTextInput from './RichTextInput'
+import SvgIcon from '@/components/SvgIcon'
+
+interface styleState {
+  fontFamily?: string
+  fontSize?: string
+  color?: string
+  backgroundColor?: string
+  fontWeight?: string
+  textAlign?: string
+}
+
+interface textState {
+  textValue: string
+  textStyles?: styleState
+  onChangeValue: (val: string) => void
+  onChangeStyles?: (val: styleState) => void
+}
+
+const alignItems: MenuProps['items'] = [
+  {
+    key: 'left',
+    label: '左对齐'
+  },
+  {
+    key: 'center',
+    label: '居中'
+  },
+  {
+    key: 'right',
+    label: '右对齐'
+  }
+]
+
+const RichTextSetting: FC<textState> = ({ textValue, textStyles, onChangeValue, onChangeStyles }) => {
+  const [value, setValue] = useState<string>(textValue)
+  const [styles, setStyles] = useState<styleState>(textStyles || {})
+
+  const onClick: MenuProps['onClick'] = ({ key }) => {
+    console.log(key)
+  }
+
+  return (
+    <Form
+      colon={false}
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 18 }}
+      labelAlign='left'
+      style={{ width: '50%', margin: '0 auto' }}
+    >
+      <Form.Item label='文本'>
+        <RichTextInput value={value} onChange={val => setValue(val)} />
+      </Form.Item>
+      <Form.Item label='字体'>
+        <Select
+          defaultValue={styles.fontFamily}
+          options={['黑体', '宋体', '楷体', '隶书', '微软雅黑', '华文行楷', '方正姚体'].map(item => ({
+            value: item,
+            label: item
+          }))}
+        />
+      </Form.Item>
+      <Form.Item label='字号'>
+        <Select
+          defaultValue={styles.fontSize}
+          options={[12, 14, 16, 18, 24, 32, 48].map(item => ({
+            value: item + 'px',
+            label: item + 'px'
+          }))}
+        />
+      </Form.Item>
+      <Form.Item label='颜色'>
+        <Space>
+          <ColorPicker>
+            <Button icon={<SvgIcon name='color-font' size={20} />} />
+          </ColorPicker>
+          <ColorPicker>
+            <Button icon={<SvgIcon name='color-bg' size={20} />} />
+          </ColorPicker>
+          <Button icon={<SvgIcon name='font-bold' size={20} />} />
+          <Dropdown menu={{ items: alignItems, onClick }} placement='bottomRight' trigger={['click']}>
+            <Button icon={<SvgIcon name='font-align' size={20} />} />
+          </Dropdown>
+        </Space>
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default RichTextSetting
--- src/views/image/image-composition.tsx
+++ src/views/image/image-composition.tsx
@@ -4,6 +4,8 @@ import { Row, Col, Card, Button, Space, Form, InputNumber, Select } from 'antd'
 import { RndNode } from '@/components/DndNode'
 import { PageWrapper } from '@/components/Page'
 import { IMAGE_COMPOSITION } from '@/settings/websiteSetting'
+import { RichTextInput, RichTextSetting } from '@/components/RichText'
+import { UploadImage } from '@/components/Upload'
 
 const ImageComposition: FC = () => {
   const [config, setConfig] = useState({
@@ -24,20 +26,50 @@ const ImageComposition: FC = () => {
     })
   }
 
+  const [value, setValue] = useState('请输入文本')
+
+  const handleSuccess = (url: string) => {}
+
   return (
     <PageWrapper plugin={IMAGE_COMPOSITION}>
       <Row gutter={12}>
         <Col span={16}>
-          <Card title='合成区域' bordered={false} bodyStyle={{ height: '550px' }}>
-            <div className='dnd-container' style={{ width: '100%', height: '500px', position: 'relative' }}>
+          <Card title='合成区域' bordered={false} bodyStyle={{ height: '600px' }}>
+            <div className='dnd-container' style={{ width: '100%', height: '550px', position: 'relative' }}>
               <RndNode element={config}>
-                <div style={{ height: '100%', background: 'red' }}></div>
+                <RichTextInput value={value} onChange={val => setValue(val)} />
               </RndNode>
             </div>
           </Card>
         </Col>
         <Col span={8}>
-          <Card title='设置区域' bordered={false} bodyStyle={{ height: '550px' }}></Card>
+          <Card title='设置区域' bordered={false} bodyStyle={{ height: '600px' }}>
+            <Form
+              colon={false}
+              labelCol={{ span: 6 }}
+              wrapperCol={{ span: 18 }}
+              labelAlign='left'
+              style={{ width: '50%', margin: '0 auto' }}
+            >
+              <Form.Item label='选择底图'>
+                <UploadImage name='选择底图' isFull onSuccess={handleSuccess} />
+              </Form.Item>
+              <Form.Item label='添加文本'>
+                <Button block style={{ width: '100%' }}>
+                  添加文本
+                </Button>
+              </Form.Item>
+              <Form.Item label='添加图片'>
+                <UploadImage name='选择图片' isFull onSuccess={handleSuccess} />
+              </Form.Item>
+              <Form.Item label='删除元素'>
+                <Button type='primary' danger style={{ width: '100%' }}>
+                  删除元素
+                </Button>
+              </Form.Item>
+            </Form>
+            <RichTextSetting textValue={value} textStyles={{}} onChangeValue={val => setValue(val)} />
+          </Card>
         </Col>
       </Row>
     </PageWrapper>
