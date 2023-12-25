8cefa94a8ebd980092119528bf425e37822c4560
baimingxuan <354372738@qq.com>
2023-12-25 10:13:19 +0800

    perf: 优化DragResize页面

--- src/views/compo/drag/drag-resize.tsx
+++ src/views/compo/drag/drag-resize.tsx
@@ -1,4 +1,6 @@
-import React, { useRef, useState } from 'react'
+import type { FC } from 'react'
+import type { Position, DraggableData } from 'react-rnd'
+import { useState } from 'react'
 import { Card } from 'antd'
 import { PageWrapper } from '@/components/Page'
 import { REACT_RND_PLUGIN } from '@/settings/websiteSetting'
@@ -11,8 +13,7 @@ interface configState {
   height: number
 }
 
-const DragResize: React.FC = () => {
-  const rectWrapper = useRef(null)
+const DragResize: FC = () => {
   const [config, setConfig] = useState<configState>({
     x: 650,
     y: 130,
@@ -20,15 +21,15 @@ const DragResize: React.FC = () => {
     height: 180
   })
 
-  const handleDragStop = (_: any, d: any) => {
+  const handleDragStop = (_e: any, data: DraggableData) => {
     setConfig({
       ...config,
-      x: d.x,
-      y: d.y
+      x: data.x,
+      y: data.y
     })
   }
 
-  const handleResize = (_e: any, _direction: any, ref: any, _delta: any, position: any) => {
+  const handleResize = (_e: any, _direction: any, ref: any, _delta: any, position: Position) => {
     setConfig({
       ...config,
       width: ref.offsetWidth,
@@ -36,23 +37,23 @@ const DragResize: React.FC = () => {
       ...position
     })
   }
-  
+
   return (
     <PageWrapper plugin={REACT_RND_PLUGIN}>
-      <Card bordered={false} bodyStyle={{padding: 0}}>
-        <div ref={rectWrapper} className='rect-wrapper' style={{width: '100%', height: '500px'}}>
+      <Card bordered={false} bodyStyle={{ padding: 0 }}>
+        <div style={{ width: '100%', height: '500px' }}>
           <Rnd
-            size={{width: config.width, height: config.height}}
-            position={{x: config.x, y: config.y}}
+            size={{ width: config.width, height: config.height }}
+            position={{ x: config.x, y: config.y }}
             minWidth={100}
             minHeight={100}
             onDragStop={handleDragStop}
             onResize={handleResize}
-            bounds='.rect-wrapper'
-            style={{background: '#1890ff'}}
+            bounds='parent'
+            style={{ background: '#1890ff' }}
           >
-            <div className='flex-center' style={{height: '100%'}}>
-              <div style={{width: '90px', color: '#fff'}}>
+            <div className='flex-center' style={{ height: '100%' }}>
+              <div style={{ width: '90px', color: '#fff' }}>
                 <p>x: {config.x}</p>
                 <p>y: {config.y}</p>
                 <p>width: {config.width}</p>
